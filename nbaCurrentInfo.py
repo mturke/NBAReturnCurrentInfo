6929e60db71cf38075eea56d17ac188c89983e3c
Mike Turke <turkem1993@gmail.com>
2019-04-11 20:00:38 -0400

    Update nbaCurrentInfo.py

--- nbaCurrentInfo.py
+++ nbaCurrentInfo.py
@@ -20,13 +20,3 @@ print()
 #print("Russell WestBrook's current AST average:", tag4.strip())
 #print()
 
-
-'''
-from lxml import html
-import requests
-response = requests.get('http://catalog.data.gov/dataset?q=&sort=metadata_created+desc')
-doc = html.fromstring(response.text)
-title = doc.cssselect('h3.dataset-heading')[0].text_content()
-print("The name of the most recently added dataset on data.gov:")
-print(title.strip())
-'''
